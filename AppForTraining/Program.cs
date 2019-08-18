93146806af3e70f3fc33f7a661a3c17bd8fc600b
Alexandr <saint12maloj@gmail.com>
2019-08-18 16:08:08 +0300

    Add project files.

---
+++ AppForTraining/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AppForTraining
+{
+    interface ITarget
+    {
+        void SayTarget();
+    }
+    public class Target : ITarget
+    {
+        public void SayTarget()
+        {
+            Console.WriteLine("I am form the Target!");
+        }
+    }
+
+    public class Adaptee
+    {
+        public void SayNewTarget ()
+        {
+            Console.WriteLine("I am from the Adaptee!");
+        }
+    }
+
+    public class Adapter : Adaptee, ITarget // case № 1
+    {
+        public void SayTarget()
+        {
+            SayNewTarget();
+        }
+    }
+
+    //public class Adapter : ITarget // case № 2
+    //{
+    //    New n = new New();
+    //    public void SayTarget()
+    //    {
+    //        n.SayNewTarget();
+    //    }
+    //}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var first = new Target();
+            first.SayTarget();
+            var second = new Adapter();
+            second.SayTarget();
+            Console.Read();
+        }
+    }
+   
+}
